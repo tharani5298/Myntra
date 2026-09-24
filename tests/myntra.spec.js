d9c57a675062be2427bc3bf8e396d1076408c278
tharani5298 <tharanirameshkumar35@gmail.com>
2026-09-24 16:43:42 +0530

    modified12

--- tests/myntra.spec.js
+++ tests/myntra.spec.js
@@ -13,7 +13,7 @@
     let Price = priceList.map((price) => Number(price.replace(/[^0-9]/g, '')));
    let minPrice = Math.min(...Price)   
      console.log('Minimumprice ' + minPrice);
-     console.log('Total Products price ' + totalProductslist);
+     console.log('Total Products' + totalProductslist);
 
  })
 
